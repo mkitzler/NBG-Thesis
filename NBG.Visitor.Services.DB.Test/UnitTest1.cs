d857c5fde11f4e5f7aa901abbeaf7b62d3ba1461
m_kitzler <martin.kitzler62@gmail.com>
2021-10-23 18:32:47 +0200

    Added VisitRepository.AddVisit, Created NBG.Visitor.Services.DB Unit Test

---
+++ NBG.Visitor.Services.DB.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace NBG.Visitor.Services.DB.Test
+{
+    public class Tests
+    {
+        private VisitRepository _repo = new();
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ src/NBG.Visitor.Services.DB/VisitRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBG.Visitor.Storage;
+using NBG.Visitor.Storage.Models;
+
+namespace NBG.Visitor.Services.DB
+{
+    public class VisitRepository
+    {
+        private VisitContext _context = new();
+
+        public async Task<Storage.Models.Visitor> GetVisitorIfExists(string firstName, string lastName, string phoneNumber)
+        {
+            return await _context.Visitors.FindAsync(firstName, lastName, phoneNumber);
+        }
+
+        /// <summary>
+        /// Adds a visit entry to the Database.
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="contactPersonName"></param>
+        /// <param name="companyLabel"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="phoneNumber"></param>
+        /// <param name="email"></param>
+        public async Task AddVisit(DateTime start, ContactPerson contactPerson, Company company, string firstName, string lastName, string phoneNumber, string email = null)
+        {
+            var visitor = await GetVisitorIfExists(firstName, lastName, phoneNumber);
+            if (visitor == null)
+            {
+                visitor = _context.Visitors.Add(new Storage.Models.Visitor() {  FirstName = firstName, LastName = lastName, PhoneNumber = phoneNumber, Email = email }).Entity;
+            }
+            _context.Visits.Add(new Visit() { VisitStart = start, Visitor = visitor, ContactPerson = contactPerson, Company = company });
+        }
+    }
+}
