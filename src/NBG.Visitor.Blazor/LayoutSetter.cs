4342f1a38e03b22aeb4bbcd234344cfab77e5fad
Martin Kitzler <m.kitzler@htlkrems.at>
2021-11-24 13:08:26 +0100

    Added carousel to TerminalView, Created TerminalSafety, Creating subclass to display footer for TerminalHome

---
+++ src/NBG.Visitor.Blazor/LayoutSetter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBG.Visitor.Blazor
+{
+    public class LayoutSetter : ComponentBase
+    {
+        [CascadingParameter]
+        public MainLayout Layout { get; set; }
+
+        [Parameter]
+        public RenderFragment Header { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Layout.SetHeaderAndFooter(Header, Footer);
+        }
+    }
+
+}
