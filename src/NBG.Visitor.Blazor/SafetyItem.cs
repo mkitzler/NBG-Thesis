775b296666c08515238f7d20d4e9cf016430b6c1
Martin Kitzler <m.kitzler@htlkrems.at>
2021-11-25 14:28:01 +0100

    Added Elements to SafetyPage, Fixed Validation of Register form

---
+++ src/NBG.Visitor.Blazor/SafetyItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBG.Visitor.Blazor
+{
+    public class SafetyItem
+    {
+        public string Text { get; set; }
+        public string ImagePath { get; set; }
+
+		public SafetyItem(string text, string imagePath)
+		{
+            Text = text;
+            ImagePath = imagePath;
+		}
+    }
+}
