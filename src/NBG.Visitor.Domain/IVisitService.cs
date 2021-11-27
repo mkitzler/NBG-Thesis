d99cb1c700656e9f49a42c861d40051d832116e9
luka <l.boahn@htlkrems.at>
2021-11-27 16:23:11 +0100

    Removed Navbar, Added Authentication for visitor overview (not finished)

--- src/NBG.Visitor.Domain/IVisitService.cs
+++ src/NBG.Visitor.Domain/IVisitService.cs
@@ -8,13 +8,9 @@ namespace NBG.Visitor.Domain
     public interface IVisitService
     {
         Task<VisitorDto> ReadVisitorIfExists(string firstName, string lastName, string phoneNumber);
-
         Task AddVisit(DateTime start, ContactPersonDto contactPerson, CompanyDto company, string firstName, string lastName, string phoneNumber, string email = null);
-
         Task<List<VisitDto>> ReadAllVisits();
-
         Task RemoveVisit(VisitDto visit);
-
         Task UpdateVisit(VisitDto visit);
     }
 }
