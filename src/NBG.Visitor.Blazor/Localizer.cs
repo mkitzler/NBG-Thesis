65cf4b3d96e172f89f69b018d76bedf335f76b4c
m_kitzler <martin.kitzler62@gmail.com>
2021-12-29 17:57:05 +0100

    Fixed Layout, added Localization

---
+++ src/NBG.Visitor.Blazor/Localizer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+using System.Reflection;
+using System.Globalization;
+
+namespace NBG.Visitor.Blazor
+{
+    public class Localizer
+    {
+        private ResourceManager rm;
+        public CultureInfo Culture { get; set; }
+
+        public Localizer(string baseName, Assembly assembly, string culture)
+        {
+            rm = new(baseName, assembly);
+            Culture = new(culture);
+        }
+
+        public string this[string toLocalize]
+        {
+            get
+            {
+                return rm.GetString(toLocalize, Culture);
+            }
+        }
+    }
+}
---
+++ src/NBG.Visitor.Blazor/RegisteredEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBG.Visitor.Blazor
+{
+    public class RegisteredEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+
+        public RegisteredEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
