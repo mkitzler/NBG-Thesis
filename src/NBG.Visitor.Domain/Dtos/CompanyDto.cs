112f4b10e5e4d8e42a34a556506ab898e3a1f8e2
lboahn <l.boahn@htlkrems.at>
2021-12-29 17:07:39 +0100

    Edit of DTOs, Dialogs

---
+++ src/NBG.Visitor.Domain/Dtos/CompanyDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBG.Visitor.Domain.Dtos
+{
+    public class CompanyDto
+    {
+        public string CompanyLabel { get; set; }
+    }
+}
---
+++ src/NBG.Visitor.Domain/Dtos/VisitStatusDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBG.Visitor.Domain.Dtos
+{
+    public enum VisitStatusDto
+    {
+        VISIT_ACTIVE,
+        VISIT_PENDING,
+        VISIT_OVER
+    }
+}
