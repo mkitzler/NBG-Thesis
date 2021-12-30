626ab186a530cda358c7f283e47c033c06b0242a
m_kitzler <martin.kitzler62@gmail.com>
2021-12-30 21:26:14 +0100

    Added resource for SafetyItems and method to load safety items, tried to fix pictures

--- src/NBG.Visitor.Blazor/Localizer.cs
+++ src/NBG.Visitor.Blazor/Localizer.cs
@@ -14,11 +14,13 @@ namespace NBG.Visitor.Blazor
         private ResourceManager rm;
         public CultureInfo Culture { get; set; }
 
-        public Localizer(string baseName, Assembly assembly, string culture)
+        public Localizer(string baseName, Assembly assembly, CultureInfo culture)
         {
             rm = new(baseName, assembly);
-            Culture = new(culture);
+            Culture = culture;
         }
+
+        public Localizer(string baseName, Assembly assembly, string culture) : this(baseName, assembly, new CultureInfo(culture)) { }
 
         public string this[string toLocalize]
         {
--- src/NBG.Visitor.Blazor/SafetyItem.cs
+++ src/NBG.Visitor.Blazor/SafetyItem.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
 
 namespace NBG.Visitor.Blazor
 {
@@ -12,11 +10,29 @@ namespace NBG.Visitor.Blazor
         public string Text { get; set; }
         public string ImagePath { get; set; }
 
+        public SafetyItem(string data)
+        {
+            string[] fields = data.Split("\n");
+            Title = fields[0];
+            Text = fields[1];
+            ImagePath = fields[2];
+        }
+
 		public SafetyItem(string title, string text, string imagePath)
 		{
             Title = title;
             Text = text;
             ImagePath = imagePath;
 		}
+
+        public static IEnumerable<SafetyItem> LoadFromResource(Assembly assembly, CultureInfo culture, string baseName = "NBG.Visitor.Blazor.Resources.SafetyItems")
+        {
+            Localizer Loc = new(baseName, assembly, culture);
+            string? data = null;
+            for (int i = 1; (data = Loc["String" + i.ToString()]) != null; i++)
+            {
+                yield return new(data);
+            }
+        }
     }
 }
