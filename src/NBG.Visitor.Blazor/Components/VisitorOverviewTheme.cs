ec420a32059ac52612f11c7d3fddf4801a6e407f
lboahn <l.boahn@htlkrems.at>
2022-03-13 17:03:49 +0100

    Reformatted scss, added scss variables to control spacing, removed unnecessary styling, added custom mudblazor theme

---
+++ src/NBG.Visitor.Blazor/Components/VisitorOverviewTheme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MudBlazor;
+
+namespace NBG.Visitor.Blazor.Components
+{
+    public static class VisitorOverviewTheme
+    {
+
+        public static MudTheme Theme = new MudTheme()
+        {
+            Palette = new Palette()
+            {
+                Primary = Colors.Red.Darken4,
+                Secondary = Colors.Red.Darken1
+            }
+        };
+
+    }
+}
